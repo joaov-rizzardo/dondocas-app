83534db48061096074b559aed017bde510100306
João Victor Domingues Rizzardo <jvitor2000@hotmail.com>
2022-11-29 13:51:07 -0300

    Alterado mascara de valor decimal para . ou invés de ,

--- src/services/Masks.js
+++ src/services/Masks.js
@@ -8,7 +8,7 @@ export function phoneMask(value){
 
 export function moneyMask(value){
     value = value.replace(/\D/g, "")
-    value = value.replace(/(\d+)(\d{2})/g, "$1,$2")
+    value = value.replace(/(\d+)(\d{2})/g, "$1.$2")
     return value
 }
 
